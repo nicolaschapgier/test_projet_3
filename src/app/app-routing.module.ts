17eb906abbb77fb4d24616d36f437c151c1a6898
Nicolas Chapgier <nchapgier@hotmail.fr>
2023-03-23 16:57:54 +0100

    add var

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClassScssComponent } from './class.scss/class.scss.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "", component : ClassScssComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
